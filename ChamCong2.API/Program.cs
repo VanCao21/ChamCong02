7b1230bd4d1b5489a5882a1600b5c83a933c35b2
VanCao21 <levancao292001@gmail.com>
2022-04-28 17:07:31 +0700

    first commit

---
+++ ChamCong2.API/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChamCong2.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConfigureLogger();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>().UseSerilog();
+                });
+        public static void ConfigureLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log/Log.txt", restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                rollingInterval: RollingInterval.Day)
+                .WriteTo.File("log/error.txt", restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning)
+                .CreateLogger();
+        }
+    }
+}
